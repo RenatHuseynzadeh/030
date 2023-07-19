e9b39c9ec7988001f766ff94971611bf3db0e59a
RenatHuseynzadeh <124487433+RenatHuseynzadeh@users.noreply.github.com>
2023-07-19 09:16:51 +0300

    030

---
+++ BomDz.js
@@ -0,0 +1,42 @@
+// 1
+console.log(window.innerHeight);
+console.log(window.innerWidth);
+// window не важно
+
+// 6,7
+console.log(screen.width);
+console.log(screen.height);
+
+// 8,9
+console.log(screen.availWidth);
+console.log(screen.availHeight);
+
+// 10
+console.log(screen.colorDepth);
+
+// 11
+console.log(+ screen.width + "x" + screen.height);
+
+// 14
+// cookieEnabled
+
+// 15
+// appCodeName
+
+// 16
+// product
+
+// 17
+// appVersion
+
+// 18,19
+// userAgent
+
+// 20
+// Language
+
+// 21
+// online
+
+// 22
+// javaEnabled()
---
+++ Bomzadaniya.js
@@ -0,0 +1,73 @@
+// 1
+// это не правильно, это попытка
+// function second(obj){
+// 
+    // for(let arr of Object.entries(obj)){
+        // if(arr.includes(Math.min(...Object.values(obj)))){
+            // return arr[0]
+        // }
+    // }
+// }
+// 
+// console.log(second({
+    // 'fi': 1,
+    // 'tw': 15,
+    // 'th': 37
+// }));
+
+// 3
+//  let hhhhh = 0
+//  var el = 0
+//  
+//  
+//   function cislo(arg){
+//   var numEl = parseInt(arg.match(/\d+/))
+//   
+//   console.log(hhhhh += numEl);
+//  }
+//  
+// 
+// let tot = 0
+// 
+// function two(numm){
+    //  for(let i in numm){
+        //  el = numm[i]
+        //  
+    //  }  
+    //  cislo(el)
+    //  
+//  }
+//  
+//  two(['five', 'wenty', 'thr33'])
+
+// 4
+// let pop = 0
+// function twoo(num2){
+    // for(let i in num2){
+        // pop += num2[i]
+        // 
+    // }  
+// 
+// }
+// 
+// if(pop > 50){
+    // console.log(true);
+// }else{
+    // console.log(true);
+// }
+// 
+// twoo({a: 20, b: 31})
+// 
+// 
+// 5
+// let n = 1
+// function five(num3){
+    // for(let i in num3){
+        // console.log(num3[i] + n);
+    // }  
+// 
+// }
+// 
+// 
+
+// five({renat: 11, raman: 14, fuad: 40})
